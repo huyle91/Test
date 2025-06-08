495c2a7c717109b71ed878528ed6a3a41c90dd28
peterxyjz <huyq09@gmail.com>
2025-06-08 13:52:33 +0700

    feat: jwt (#8)

--- InfertilityTreatment.API/Controllers/AuthController.cs
+++ InfertilityTreatment.API/Controllers/AuthController.cs
@@ -9,7 +9,7 @@ using System.Security.Claims;
 namespace InfertilityTreatment.API.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("api/auth")]
     public class AuthController : ControllerBase
     {
         private readonly IAuthService _authService;

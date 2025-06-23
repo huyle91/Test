9247e4634b6109db2a109d287514e6d86311d638
Lê Minh Trí <trilm189@gmail.com>
2025-06-23 19:53:51 +0700

    fix mini bug

--- InfertilityTreatment.Data/Configurations/MedicationConfiguration.cs
+++ InfertilityTreatment.Data/Configurations/MedicationConfiguration.cs
@@ -1,11 +1,6 @@
 ﻿using InfertilityTreatment.Entity.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace InfertilityTreatment.Data.Configurations
 {
@@ -14,12 +9,28 @@ namespace InfertilityTreatment.Data.Configurations
         public void Configure(EntityTypeBuilder<Medication> builder)
         {
             builder.HasKey(m => m.Id);
+
             builder.HasIndex(m => m.Name);
             builder.HasIndex(m => m.ActiveIngredient);
-            builder.Property(m => m.Manufacturer).HasMaxLength(200);
-            builder.Property(m => m.Description).HasMaxLength(500);
-            builder.Property(m => m.StorageInstructions).HasMaxLength(500);
-            builder.Property(m => m.SideEffects).HasMaxLength(500);
+
+            builder.Property(m => m.Name)
+                   .IsRequired()
+                   .HasMaxLength(200);
+
+            builder.Property(m => m.ActiveIngredient)
+                   .HasMaxLength(200);
+
+            builder.Property(m => m.Manufacturer)
+                   .HasMaxLength(200);
+
+            builder.Property(m => m.Description)
+                   .HasMaxLength(500);
+
+            builder.Property(m => m.StorageInstructions)
+                   .HasMaxLength(500);
+
+            builder.Property(m => m.SideEffects)
+                   .HasMaxLength(500);
 
             builder.HasMany(m => m.Prescriptions)
                    .WithOne(p => p.Medication)
